9c2b5273bb348a794d1e761859ad5a748f4ddcaa
HunterX-7 <fraquin7@gmail.com>
2022-11-25 16:37:39 -0500

    Add the logic in the main.js file for the Speakers card to be generate dynamically and adjust the page responsiveness

--- main.js
+++ main.js
@@ -4,35 +4,93 @@ const obj = [{
   name: 'Miranda Torres',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker1.jpg'],
-  Job: 'President of Fashionista America',
+  job: 'President of Fashionista America',
 },
 {
   name: 'Alexa Gomez',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker2.jpg'],
-  Job: 'Owner of an Online Boutique Store',
+  job: 'Owner of an Online Boutique Store',
 },
 {
   name: 'Tesha Summers',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker3.jpg'],
-  Job: 'Fashion, Styles and Trend Expert',
+  job: 'Fashion, Styles and Trend Expert',
 },
 {
   name: 'Pia Cannavaro',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker8.jpg'],
-  Job: 'Top Fashion Model and Teacher',
+  job: 'Top Fashion Model and Teacher',
 },
 {
   name: 'Monica Gutierrez',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker5.jpg'],
-  Job: 'Shoes and Accesories Expert',
+  job: 'Shoes and Accesories Expert',
 },
 {
   name: 'Cindy Chow',
   description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
   image: ['Images/JPEG/Speaker6.jpg'],
-  Job: 'Juvenile Fashion and Style Expert',
+  job: 'Juvenile Fashion and Style Expert',
 }];
+
+// Speaker-section cards
+
+const speakerSectionMobile = document.getElementById('Speakers-mobile');
+
+for (let i = 0; i < 2; i += 1) {
+  const div = document.createElement('div');
+  div.classList.add('col-12');
+  div.innerHTML = `
+                        <div class="box">
+                            <div class="row">
+                                <div class="col-5 my-auto"><img class="img-thumbnail bg-img-thumb2" src="${obj[i].image}" alt="Featured-Speaker-${i + 1}"></div>
+                                <div class="col-7 my-auto">
+                                    <div class="row">
+                                        <div class="col-12"><h4 class="speaker-h4">${obj[i].name}</h4></div>
+                                        <div class="col-12">
+                                            <p class="speaker-p-a">${obj[i].job}</p>
+                                            <hr class="hr-speaker-desk">
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="speaker-p-b">${obj[i].description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+  speakerSectionMobile.appendChild(div);
+}
+
+const speakerSection = document.getElementById('Speakers-desk');
+
+for (let i = 0; i < obj.length; i += 1) {
+  const div = document.createElement('div');
+  div.classList.add('col-6');
+  div.innerHTML = `
+                        <div class="box">
+                            <div class="row">
+                                <div class="col-5 my-auto">
+                                    <img class="img-thumbnail bg-img-thumb2" src="${obj[i].image}" alt="Featured-Speaker-${i + 1}">
+                                </div>
+                                <div class="col-7 my-auto">
+                                    <div class="row">
+                                        <div class="col-12"><h4 class="speaker-h4">${obj[i].name}</h4></div>
+                                        <div class="col-12">
+                                            <p class="speaker-p-a">${obj[i].job}</p>
+                                            <hr class="hr-speaker-desk">
+                                        </div>
+                                        <div class="col-12">
+                                            <p class="speaker-p-b">${obj[i].description}</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    `;
+  speakerSection.appendChild(div);
+}
