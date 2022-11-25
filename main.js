9b6cc6eea0284b6466d6976d3a52501099e893ab
HunterX-7 <fraquin7@gmail.com>
2022-11-25 15:47:02 -0500

    Finish designing the Home Page and Styling the Speaker Section
    Add responsiveness to the Speaker section

---
+++ main.js
@@ -0,0 +1,38 @@
+// Obj cards
+
+const obj = [{
+  name: 'Miranda Torres',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker1.jpg'],
+  Job: 'President of Fashionista America',
+},
+{
+  name: 'Alexa Gomez',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker2.jpg'],
+  Job: 'Owner of an Online Boutique Store',
+},
+{
+  name: 'Tesha Summers',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker3.jpg'],
+  Job: 'Fashion, Styles and Trend Expert',
+},
+{
+  name: 'Pia Cannavaro',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker8.jpg'],
+  Job: 'Top Fashion Model and Teacher',
+},
+{
+  name: 'Monica Gutierrez',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker5.jpg'],
+  Job: 'Shoes and Accesories Expert',
+},
+{
+  name: 'Cindy Chow',
+  description: 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corrupti, eos.',
+  image: ['Images/JPEG/Speaker6.jpg'],
+  Job: 'Juvenile Fashion and Style Expert',
+}];
